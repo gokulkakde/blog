7fa94594f00eedb81e267b5a2480e2422c86c9da
gokulkakde <150057016+gokulkakde@users.noreply.github.com>
2025-04-27 13:38:53 +0530

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-define('ROOT_URL', 'http://localhost/blog/');
+define('ROOT_URL', 'https://blog-m220.onrender.com/');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'gokul');
 define('DB_PASS', 'hatontheboy');
